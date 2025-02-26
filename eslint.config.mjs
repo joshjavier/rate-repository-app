194095bfc6d4145b369c2ac8763c5504f0fadccf
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-02-26 18:56:13 +0800

    Set up ESLint (E10.2)

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactNative from 'eslint-plugin-react-native';
+import { fixupPluginRules } from '@eslint/compat';
+import babelParser from '@babel/eslint-parser';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['./src/**/*.{js,jsx}', 'App.js'],
+    plugins: {
+      react,
+      'react-native': fixupPluginRules({ rules: reactNative.rules }),
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...reactNative.environments['react-native'].globals,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.flat.recommended.rules,
+      ...react.configs.flat['jsx-runtime'].rules,
+      ...reactNative.configs.all.rules,
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-native/sort-styles': 'off',
+      'react-native/no-color-literals': 'off',
+    },
+  },
+];
